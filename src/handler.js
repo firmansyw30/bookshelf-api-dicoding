1e66dc3e3bad9de85268be54fe95fa80b47b57ac
Firmansyah Wicaksono <firmansyahwicaksono30@gmail.com>
2024-05-26 11:05:01 +0700

    third commit to push file

--- src/handler.js
+++ src/handler.js
@@ -123,56 +123,56 @@ const getBooksByIdHandler = (request, h) => {
     
 const editBooksByIdHandler = (request, h) => {
     const {id} = request.params;
-    const { 
-        name, year, author, 
-        summary, publisher, pageCount, 
-        readPage, reading 
+    const {
+      name, year, author,
+      summary, publisher, pageCount,
+      readPage, reading,
     } = request.payload;
-
-    if (!name) {
-        return h.response({
-            status: 'fail',
-            message: 'Gagal memperbarui buku. Mohon isi nama buku',
-        }).code(400);
-    }
-
-    if (readPage > pageCount) {
-        const response = h.response({
-            status: 'fail',
-            message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
-        });
-        response.code(400);
-        return response;
-    }
-
+    
     const updatedAt = new Date().toISOString();
     const index = books.findIndex((book) => book.id === id);
-
-    if (index !== -1) {
-        books[index] = {
-            ...books[index],
-            name, year, author, summary, 
-            publisher, pageCount, readPage, 
-            finished: pageCount === readPage, 
-            reading, updatedAt
-        };
-
-        const response = h.response({
-            status: 'success',
-            message: 'Buku berhasil diperbarui',
-        });
-        
-        response.code(200);
-        return response;
+  
+    if (name === undefined) {
+      const response = h.response({
+        status: 'fail',
+        message: 'Gagal memperbarui buku. Mohon isi nama buku',
+      });
+      response.code(400);
+      return response;
+    
+    } else if (readPage > pageCount) {
+      const response = h.response({
+        status: 'fail',
+        message:
+          'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount',
+      });
+      response.code(400);
+      return response;
+  
+    } else if (index !== -1) {
+      books[index] = {
+        ...books[index],
+        name, year, author,
+        summary, publisher,
+        pageCount, readPage,
+        reading, updatedAt,
+      };
+      const response = h.response({
+        status: 'success',
+        message: 'Buku berhasil diperbarui',
+      });
+      response.code(200);
+      return response;
+    
+    } else {
+      const response = h.response({
+        status: 'fail',
+        message: 'Gagal memperbarui buku. Id tidak ditemukan',
+      });
+      response.code(404);
+      return response;
     }
-        const response = h.response({
-            status: 'fail',
-            message: 'Gagal memperbarui buku. Id tidak ditemukan',
-        });
-        
-        response.code(404);
-        return response;
-};
+  };
 
 const deleteBooksByIdHandler = (request, h) => {
     const {id} = request.params;
